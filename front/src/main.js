3be2a9c6a0043337fb44189ee9977934d15ea5c3
NoasProject <lowntry@gmail.com>
2021-12-26 14:37:39 +0900

    main js

--- front/src/main.js
+++ front/src/main.js
@@ -1,4 +1,29 @@
-import { createApp } from 'vue'
+import Vue from "vue";
+import vueCookie from "vue-cookies";
 import App from './App.vue'
+import router from "./router";
+import axios from "axios";
+import vueAxios from "vue-axios";
+import bootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false;
+
+Vue.use(vueCookie);
+Vue.use(vueAxios, axios);
+Vue.use(bootstrapVue);
+
+const vue = new Vue({
+    router: router,
+    render: (h) => h(App),
+}).$mount("#app");
+
+export default {
+    vue,
+    created() {
+
+    },
+    methods: {
+    }
+};
